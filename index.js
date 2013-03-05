bcb67ca46ad534a5d2cc9c195344edf00a58622f
Dominik Burgdörfer <dominik.burgdoerfer@mikanda.de>
2013-03-05 01:38:56 +0100

    Initial version.

---
+++ index.js
@@ -0,0 +1,57 @@
+var Stream = require('stream'),
+    inherits = require('util').inherits,
+    Buffer = require('buffer').Buffer;
+
+/**
+ * Exports methods which implement the stream interface and operate on a
+ * string.
+ * @param {String} data A initial string to fill the buffer.
+ */
+
+function Readable(data) {
+  Readable.super_.call(this);
+  
+  /*! The implementation works with a buffer internally. */
+
+  if (!Buffer.isBuffer(data)) {
+    
+    /*! In case that we don't get a buffer as argument we convert it into a
+     * unicode string. */
+
+    this._data = new Buffer(data);
+  } else {
+    this._data = data || '';
+  }
+};
+inherits(Readable, Stream);
+
+/**
+ * Reads the given n bytes from the stream.
+ * @param {Number} n Number of bytes to read.
+ */
+
+Readable.prototype.read = function (n) {
+  var chunk;
+
+  /*! We must take care: The buffer doesn't like .slice(0, -1) */
+
+  if (n === -1 || n == null) {
+    chunk = this._data;
+  } else {
+    chunk = this._data.slice(0, n);
+  }
+  if (n >= this._data.length || n === -1) this.emit('end');
+  return chunk;
+};
+
+/**
+ * Not quite sure if it is necessary to implement the pipe method.  But the
+ * piping with the core method doesn't work. 
+ * @param {Buffer} dest The destination stream to write to.
+ */
+
+Readable.prototype.pipe = function (dest) {
+  dest.write(this.read());
+  dest.end();
+};
+exports.Readable = Readable;
---
+++ test/index.js
@@ -0,0 +1,21 @@
+var Readable = require('..').Readable,
+    Pipe = require('pipette').Pipe;
+require('should');
+describe('Readable stream', function () {
+  it('should work', function (done) {
+    var stream = new Readable('Test buffer'),
+        pipe = new Pipe();
+    pipe.reader.on('end', function (data) {
+      done();
+    });
+    pipe.reader.on('error', done);
+    pipe.reader.on('data', function (data) {
+      try {
+        data.toString().should.equal('Test buffer');
+      } catch (x) {
+        done(x);
+      }
+    });
+    stream.pipe(pipe.writer);
+  });
+});
